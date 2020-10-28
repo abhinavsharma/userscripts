44f8f37be9648fceb6f9e3778e0d38febc11f6b2
Abhinav Sharma <abhinav@laso.ai>
2020-10-28 11:16:29 -0700

    Create youtubepip.js

---
+++ youtubepip.js
@@ -0,0 +1,6 @@
+(function() {
+  let v = document.querySelector('video');
+  v.addEventListener('webkitpresentationmodechanged', (e)=>e.stopPropagation(), true);
+  setTimeout(()=>v.webkitSetPresentationMode('picture-in-picture'), 3000);
+  completion()
+})()
