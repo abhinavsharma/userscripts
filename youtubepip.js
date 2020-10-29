c77c4f14cf8eee5fd9dee6067992516d2aa761e7
Abhinav Sharma <abhinav@laso.ai>
2020-10-29 15:17:15 -0700

    Update youtubepip.js

--- youtubepip.js
+++ youtubepip.js
@@ -1,6 +1,15 @@
 (function() {
-  let v = document.querySelector('video');
-  v.addEventListener('webkitpresentationmodechanged', (e)=>e.stopPropagation(), true);
-  setTimeout(()=>v.webkitSetPresentationMode('picture-in-picture'), 3000);
-  completion()
+  let url = null;
+  function act() {
+    if (window.location.href != url) {
+      let v = document.querySelector('video');
+      v.addEventListener('webkitpresentationmodechanged', (e)=>e.stopPropagation(), true);
+      setTimeout(()=>v.webkitSetPresentationMode('picture-in-picture'), 3000);
+      completion()
+      url = window.location.href;
+    }
+  }
+  
+  act();
+  setInterval(act, 1000);
 })()
