e17c0d44adc3177c9671c3d345be8b56aba3b5f6
Abhinav Sharma <abhinav@laso.ai>
2020-10-30 07:23:26 -0700

    Update redditupsell.js

--- redditupsell.js
+++ redditupsell.js
@@ -1,9 +1,23 @@
 (function() {
   setInterval(function() {
-    var elem = document.querySelector('#continueButton');
-    if(elem) {
-        elem.click()
+    
+    function ex() {
+      var elem = document.querySelector('#continueButton');
+      if(elem) {
+          elem.click()
+      }
     }
+
+    Array.from(document.querySelectorAll('iframe')).forEach((e) => {
+        if (e) {
+            try {
+                e.contentWindow.eval(ex.toString())
+                e.contentWindow.ex()
+            } catch {}
+        }
+    })
+    
+    
   }, 250);
   
 })();
